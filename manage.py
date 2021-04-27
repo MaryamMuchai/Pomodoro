717a959c22ded267e88c13bfbf6f43e503f4d9c3
Marya <maryammuchai@gmail.com>
2021-04-27 17:08:44 +0300

    created a user model

--- manage.py
+++ manage.py
@@ -2,6 +2,8 @@ from app import create_app,db
 from flask_script import Manager,Server
 from flask_migrate import Migrate, MigrateCommand
 
+from app.models import User
+
 #Creating app instance
 app = create_app('production')
 app = create_app('test')
@@ -21,7 +23,7 @@ def test():
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db)
+    return dict(app = app,db = db, user= user)
     
 if __name__ == '__main__':
     manager.run()
