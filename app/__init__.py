ff05e7a8d656c89e4cc9581254081637596d3374
Marya <maryammuchai@gmail.com>
2021-04-27 16:49:41 +0300

    importing the flask migrate

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask 
+from flask import Flask
 from config import config_options
 
 from flask_bootstrap import Bootstrap
@@ -10,12 +10,13 @@ db = SQLAlchemy()
 bootstap = Bootstrap()
 
 
-
-
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config_options[config_name])
 
+    #initializing flask extensions
     bootstap.init_app(app)
+    db.init_app(app)
+
    
     return app
