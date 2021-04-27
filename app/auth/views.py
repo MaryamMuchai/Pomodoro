ac6e40f875516c69c26d846660429ef7a485fb83
Marya <maryammuchai@gmail.com>
2021-04-27 18:50:04 +0300

    created custom validators

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,8 +1,12 @@
-from flask import render_template ,request,redirect
+from flask import render_template ,request,redirect, url_for
 from . import auth
+from .forms import RegistrationForm
+from .models import User
+from ..import db
 
 @auth.route('/login')
 def login():
     return
     render_template('auth/login.html')
 
+@auth
